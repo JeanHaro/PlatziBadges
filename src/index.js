0b560f7177f316c3c386f2bf2085646f42c314da
Jean Haro <JeanCHL98@hotmail.com>
2021-07-31 19:15:30 -0500

    jsx y React.createElement

--- src/index.js
+++ src/index.js
@@ -26,8 +26,61 @@ import React from 'react'; // Será el análogo al createElement
 // importamos ReactDOM que nos vamos a traer desde react-dom
 import ReactDOM from 'react-dom'; // Será el análogo al appendChild
 
+// Otra alternativa es ====
+// React.createElement
+// Primer parámetro es el tipo de elemento
+// Segundo parámetro es son los atributos(En react se llaman props o propertys)
+// Tercer atributo es el children(parecido al innerText o innerHTML)
+/* const element = React.createElement('h1', {}, 'Hola Soy los children.');
+*/
+
+// ------------------------------------------------------------
+
+// Con atributo ====
+/* const enlace = React.createElement('a', { href: 'https://platzi.com' }, 'Ir a Platzi'); */
+
+// ------------------------------------------------------------
+
+// Con variables ====
+/* const name = 'Jean';
+const element = React.createElement('h1', {}, `Hola, soy ${name}`); */
+
+// ------------------------------------------------------------
+
 // JSX - por esto importamos React
-const element = <h1>Hello, Platzi Badges from React</h1>
+/* const jsx = <h1>Hello, Platzi Badges from React</h1> */
+
+// ------------------------------------------------------------
+
+const name = 'Jean';
+const jsx = <h1>Hola soy, {name}</h1> // Hola soy, Jean
+
+// ------------------------------------------------------------
+
+const jsx2 = <h1>Hola soy, {2 + 2}</h1> // Hola soy, 4
+
+// ------------------------------------------------------------
+
+const sum = () => 3 + 3;
+const jsx3 = <h1>Hola soy, {sum()}</h1> // Hola soy, 6
+
+// ------------------------------------------------------------
+
+const jsx4 = (
+    <div>
+        <h1>Hola, soy Jean</h1>
+        <p>Soy un desarrollador</p>
+    </div>
+);
+
+// ------------------------------------------------------------
+
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'Hola, soy Jean'),
+    React.createElement('p', {}, 'Soy un desarrollador')
+);
 
 // Este si será igual que arriba
 const container = document.getElementById('app');
@@ -36,4 +89,4 @@ const container = document.getElementById('app');
 // El ReactDOM utilizando el metodo render es como usar appendChild
 // En donde en el primer parámetro será que elemento queremos posicionar
 // Y en segundo parámetro es en donde queremos posicionar ese elemento
-ReactDOM.render(element, container); 
+ReactDOM.render(element, container);
